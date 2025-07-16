87c3a3515287e6ae856b474a6733c4a7ce3deddf
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-07-16 19:00:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ io.on('connection', (socket) => {
 
 
 app.use(cors({
-    origin: 'http://localhost:5173',  // Allow all origins
+    origin: 'http://localhost:5173, https://pm-2.netlify.app',  // Allow all origins
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],  // Allow all HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization']  // Allow necessary headers
 }));
