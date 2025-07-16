189c399a8ce65a3b98cdaba78e290595e7e526df
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-07-16 19:22:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.set('views', path.join(__dirname, 'views')); // Ensure correct path to views
 const server = http.createServer(app); // wrap express app with http server
 const io = socketIo(server, {
   cors: {
-    origin: ['http://localhost:5173, https://pm-2.netlify.app'], // Allow all origins for development
+    origin: ['http://localhost:5173', 'https://pm-2.netlify.app'], // Allow all origins for development
     methods: ['GET', 'POST', 'PUT', 'DELETE']
   }
 });
@@ -60,7 +60,7 @@ io.on('connection', (socket) => {
 
 
 app.use(cors({
-    origin: ['http://localhost:5173, https://pm-2.netlify.app'],  // Allow all origins
+    origin: ['http://localhost:5173', 'https://pm-2.netlify.app'],  // Allow all origins
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],  // Allow all HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization']  // Allow necessary headers
 }));
