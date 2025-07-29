0b4d0602f733f631b0e1f3b5cf14c19c225febd5
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-07-29 17:10:39 +0530

    Update feedbackRoutes.js

--- routes/feedbackRoutes.js
+++ routes/feedbackRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { addFeedback, getAllFeedback, getFeedbackById, updateFeedback, deleteFeedback, getProjectStatusReport, getFeedbackLog } = require('../controller/feedback');
+const { addFeedback, getAllFeedback, getFeedbackById, updateFeedback, deleteFeedback, getProjectStatusReport, getFeedbackLog, getTeamPerformance } = require('../controller/feedback');
 const authMiddleware = require('../middleware/authMiddleware');
 const multer = require('multer');
 const path = require('path');
@@ -14,6 +14,7 @@ router.patch('/updateFeedback/:id', authMiddleware, updateFeedback);
 router.delete('/deleteFeedback/:id', authMiddleware, deleteFeedback);
 router.get('/getProjectStatusReport', authMiddleware, getProjectStatusReport);
 router.get('/getFeedbackLog', authMiddleware, getFeedbackLog);
+router.get('/getTeamPerformance', authMiddleware, getTeamPerformance);
 
 
 
