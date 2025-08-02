8c3dacea44d4b9cd62e53cf298eaf2a7a11eda86
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-08-02 17:44:19 +0530

    Update projectRoutes.js

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { addProject, getAllProjects, getProjectById, updateProject, deleteProject, getProjectsByStatus, getFilteredProjects } = require('../controller/project');
+const { addProject, getAllProjects, getProjectById, updateProject, deleteProject, getProjectsByStatus, getFilteredProjects, getProjectsByManagerId } = require('../controller/project');
 const authMiddleware = require('../middleware/authMiddleware');
 const multer = require('multer');
 const path = require('path');
@@ -7,12 +7,13 @@ const path = require('path');
 const router = express.Router();
 
 router.post('/addProject', authMiddleware, addProject);
-router.get('/getAllProjects', authMiddleware,  getAllProjects);
-router.get('/getProjectById/:id', authMiddleware, getProjectById);
+router.get('/getAllProjects',  getAllProjects);
+router.get('/getProjectById/:id', getProjectById);
 router.get('/getProjectsByStatus', authMiddleware, getProjectsByStatus);
 router.get('/getFilteredProjects', authMiddleware,  getFilteredProjects);
 router.patch('/updateProject/:id', authMiddleware, updateProject);
 router.delete('/deleteProject/:id', authMiddleware, deleteProject);
+router.get('/getProjectsByManagerId/:projectManagerId', getProjectsByManagerId);
 
 
 module.exports = router;
