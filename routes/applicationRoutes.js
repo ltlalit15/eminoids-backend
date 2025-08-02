805d279a02ae023a2f5506ef54ab1a07986dd7b1
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-08-02 13:29:54 +0530

    Update applicationRoutes.js

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { addApplication, getAllApplication } = require('../controller/application');
+const { addApplication, getAllApplication, updateApplicationById, deleteApplicationById } = require('../controller/application');
 const authMiddleware = require('../middleware/authMiddleware');
 const multer = require('multer');
 const path = require('path');
@@ -10,9 +10,8 @@ const router = express.Router();
 router.post('/addApplication', authMiddleware, addApplication);
 router.get('/getAllApplication', authMiddleware, getAllApplication);
 // router.get('/getAgreementById/:id', getAgreementById);
-// router.patch('/updateAgreement/:id', updateAgreement);
-// router.delete('/deleteAgreement/:id', deleteAgreement);
-
+router.patch('/updateApplicationById/:id', updateApplicationById);
+router.delete('/deleteApplicationById/:id', deleteApplicationById);
 
 
 
