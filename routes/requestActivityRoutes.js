534b6584df83de67d53a38f1060737ddf2f6aa43
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-08-02 16:31:15 +0530

    Update requestActivityRoutes.js

--- routes/requestActivityRoutes.js
+++ routes/requestActivityRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { addActionRequest, getPendingRequests, updateRequestStatus } = require('../controller/requestActivity');
+const { addActionRequest, getPendingRequests, updateRequestStatus, getAllReviewedRequests } = require('../controller/requestActivity');
 const authMiddleware = require('../middleware/authMiddleware');
 const multer = require('multer');
 const path = require('path');
@@ -11,6 +11,7 @@ const router = express.Router();
 router.post('/addActionRequest', addActionRequest);
 router.get('/getPendingRequests', getPendingRequests);
 router.put('/updateRequestStatus/:id', updateRequestStatus);
+router.get('/getAllReviewedRequests', getAllReviewedRequests);
 
 
 module.exports = router;    
